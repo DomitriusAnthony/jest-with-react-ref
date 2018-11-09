dfae05920608c940d125a6f02f9efd72cb43255d
Domitrius Clark <domitriusaclark@gmail.com>
2018-11-09 17:04:13 -0500

    Added a test for form submit on login.

--- notes.js
+++ notes.js
@@ -5,7 +5,7 @@
 ******* Here will be notes that will be important references while building your tests *******
 
 
-A test structure looks like:
+- A test structure looks like:
 
 test('title', () => {
     // Arrange
@@ -15,6 +15,16 @@ test('title', () => {
     // Assert
 });
 
+
+- Throw logging the submit of your forms 
+throw new Error(JSON.stringify({newPost}, null, 2));
+
+- Flush promises 
+const flushPromises = () => {
+    return new Promise(resolve => {
+        setTimeout(resolve, 0);
+    });
+};
 
 
 
